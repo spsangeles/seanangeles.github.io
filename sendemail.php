7955f261701fbb218ad5737e2dc3c2c78bd02101
spsangeles <71079527+spsangeles@users.noreply.github.com>
2020-09-11 05:07:22 +0800

    V 0.22

---
+++ sendemail.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+<p>
+<span>Email: <?php echo $_POST["myemail"]; ?></span>
+<span>Name: <?php echo $_POST["myname"]; ?></span>
+<span>Message: <?php echo $_POST["mymessage"]; ?></span>
+</p>
+?>
+
+</body>
+</html>
